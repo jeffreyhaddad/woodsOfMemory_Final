c55f1c9b289f200f6ed189c766dd4c2aef647c54
anthonydiab1 <anthony.aldiab@std.balamand.edu.lb>
2026-02-20 21:01:48 +0200

    h

--- Assets/Scripts/Interaction/FinalBoneFireScript.cs
+++ Assets/Scripts/Interaction/FinalBoneFireScript.cs
@@ -1,19 +1,55 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class FinalBoneFireScript : MonoBehaviour
 {
+
+    Inventory inventory;
     public GameObject player;
 
+    public ParticleSystem[] particuleSystems;
     public ParticleSystem fireEffect;
-
     public ParticleSystem smokeEffect;
-
     public ParticleSystem lightEffect;
 
     public bool isLit;
 
+    public bool isPlayerInRange;
+
     public void Start()
     {
-        player = get
+        inventory = FindAnyObjectByType<Inventory>();
+        particuleSystems = GetComponentsInChildren<ParticleSystem>();
+        fireEffect = particuleSystems[0];
+        smokeEffect = particuleSystems[2];
+        lightEffect = particuleSystems[1];
+        isLit = false;
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerInRange = true;
+        }
+    }
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerInRange = false;
+        }
+    }
+    public void Update()
+    {
+        ItemData wood = ItemRegistry.Get("Wood");
+        ItemData stone = ItemRegistry.Get("Stone");
+        if (isPlayerInRange && Input.GetKeyDown(KeyCode.E) && inventory.HasItem(wood, 3) && inventory.HasItem(stone,2) && !isLit)
+        {
+            fireEffect.Play();
+            lightEffect.Play();
+            smokeEffect.Play();
+            isLit = true;
+
+        }
     }
 }

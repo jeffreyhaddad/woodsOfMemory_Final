94f789f6623f29496fa9d946535fffd7314e1485
anthonydiab1 <anthony.aldiab@std.balamand.edu.lb>
2026-02-20 19:32:45 +0200

    Working bonfire and second cabin part1

---
+++ Assets/Scripts/Interaction/FinalBoneFireScript.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class FinalBoneFireScript : MonoBehaviour
+{
+    public GameObject player;
+
+    public ParticleSystem fireEffect;
+
+    public ParticleSystem smokeEffect;
+
+    public ParticleSystem lightEffect;
+
+    public bool isLit;
+
+    public void Start()
+    {
+        player = get
+    }
+}

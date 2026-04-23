a8b0ae515979e4ed6d3526c993e41275bc1d6f3e
jefreyhaddad <haddadj3ffr3y@gmail.com>
2026-04-23 12:48:54 +0300

    Remove leftover debug log from SwimmableArea, clean up deleted meta

--- Assets/Scripts/Interaction/SwimmableArea.cs
+++ Assets/Scripts/Interaction/SwimmableArea.cs
@@ -18,17 +18,6 @@ public class SwimmableArea : MonoBehaviour
     private void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Player"))
-        {
             PlayerMovement.isInwater = false;
-            Debug.Log("Here !!");
-        }
-        
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
